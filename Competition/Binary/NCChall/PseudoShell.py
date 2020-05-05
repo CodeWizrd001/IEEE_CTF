edb28994eee59ec373ed2d712eb76fee36a0bac7
Ezio <ezio@kali.kali.com>
2020-05-05 22:45:45 +0530

    Minor Bugfix in Pseudo Shell

--- Competition/Binary/NCChall/PseudoShell.py
+++ Competition/Binary/NCChall/PseudoShell.py
@@ -13,18 +13,21 @@ class ShellServer(Server) :
             client = cl
             userp = "temp-user-"+addr[0].replace('.','-')+"@ieeectf:~$ "
             userp = userp.encode()
-            denied = "bash: {} : Command Not Found\n"
-            err = "bash: {} : Permission Denied\n"
+            denied = "bash: {}: Command not found\n"
+            err = "bash: {}: Permission denied\n"
+            dirr = "bash: {}: No such file or directory\n"
             client.send("""
 Custom Shell Server With Limited Functionality
 
 New User Login from {} at {}
             \n""".format(addr[0],time.ctime()).encode())
             shellin = "" 
-            while shellin != "exit" and shellin != "exit " and shellin !="exit  " and shellin !="exit   ":
+            while True:
                 client.send(userp)
                 shellin = client.recv(2048).decode().strip('\n')
-                if shellin == "" :
+                if shellin != "exit" or shellin != "exit " or shellin !="exit  " or shellin !="exit   " :
+                    break
+                elif shellin == "" :
                     continue
                 elif shellin == "ls" or shellin == "ls " or shellin == "ls  " :
                     client.send("{}\t{}\n".format("vuln","flag.txt").encode())
@@ -40,6 +43,8 @@ New User Login from {} at {}
                     client.send(err.format(shellin.split()[0]).encode())
                 elif shellin.split()[0] == "./flag.txt" :
                     client.send(err.format(shellin.split()[0]).encode())
+                elif shellin[:2] == './' :
+                    client.send(dirr.format(shellin.split()[0]).encode())
                 else :
                     client.send(denied.format(shellin.split()[0]).encode())
                     continue
